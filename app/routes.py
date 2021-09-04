e788b37ced1e6603653de4e9aaddd5094eb71e1b
dedeza <80919791+dedeza@users.noreply.github.com>
2021-09-04 16:19:17 -0300

    update

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Olá Andreza!"
+
+    usuario = {"nome": "Andreza"}
+
+    return render_template('index.html', usuario = usuario, titulo = 'Página inicial')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html', titulo='Microblog - contato')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html', titulo='Microblog - sobre')
